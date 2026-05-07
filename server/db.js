7c88056be8461c3c5426529a0340196e5c7481e4
Thai Tran <tnmthai@gmail.com>
2026-05-08 10:41:35 +1200

    Disable SSL for postgres (Railway internal network)

--- server/db.js
+++ server/db.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
-const isRailway = !!process.env.RAILWAY_ENVIRONMENT_NAME;
-const dbUrl = process.env.DATABASE_URL || '';
-
 const pool = new Pool({
-  connectionString: dbUrl,
-  ssl: dbUrl.includes('railway.internal') ? false : (isRailway ? { rejectUnauthorized: false } : false),
+  connectionString: process.env.DATABASE_URL,
+  ssl: false,
 });
 
 module.exports = pool;
