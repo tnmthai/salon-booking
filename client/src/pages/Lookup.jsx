27217266f80c3d86cfe706e8930ce49449842f9f
Thai Tran <tnmthai@gmail.com>
2026-05-10 12:10:26 +1200

    Fix: missing import in Lookup.jsx causing white screen

--- client/src/pages/Lookup.jsx
+++ client/src/pages/Lookup.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Link, useSearchParams } from 'react-router-dom'
+import { api, getSalonTimezone } from '../utils/api'
 
 const TZ = getSalonTimezone()
 
